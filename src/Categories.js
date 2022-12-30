489e031505a2e84888b8ba62436293d4ba6b0e48
SadiKISLAR <sadi22kislar@gmail.com>
2022-12-30 15:01:03 +0300

    buttons done dynamic

--- src/Categories.js
+++ src/Categories.js
@@ -1,20 +1,14 @@
 import React from 'react';
 
-const Categories = ({ filterItems }) => {
+const Categories = ({ categories, filterItems }) => {
   return <div className='btn-container'>
-    <button className="filter-btn" onClick={() => filterItems('all')}>
-      All
-    </button>
-    <button className="filter-btn" onClick={() => filterItems('breakfast')}>
-      breakfast
-    </button>
-    <button className="filter-btn" onClick={() => filterItems('lunch')}>
-      lunch
-    </button>
-    <button className="filter-btn" onClick={() => filterItems('shakes')}>
-      shakes
-    </button>
-  </div>;
+    {categories.map((category, index) => {
+      return <button type='button' className='filter-btn' key={index} onClick={() => filterItems(category)}>
+        {category}
+      </button>
+
+    })}
+  </div >;
 };
 
 export default Categories;
